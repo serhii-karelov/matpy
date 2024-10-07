6d2e4386f1f9ca2d4874695a934f3e9dff06b707
Serhii Karelov <serhii@matmul.org>
2024-10-07 12:22:05 +0700

    Trace memory allocation

--- measure.py
+++ measure.py
@@ -1,3 +1,5 @@
+import tracemalloc
+
 from timeit import timeit
 
 import matpy
@@ -18,12 +20,36 @@ def measure_matpy(times=1):
     time_add = (timeit(lambda: m1 + m2, number=times) / times) 
     return  time_mmul, time_add
 
+def report_memory_vanilla():
+    print("### Memory allocations of pure-Python implementation ###")
+    tracemalloc.start()
+    m1 = matrix.Matrix.rand(2048 * 2, 2048 * 2)
+    snap = tracemalloc.take_snapshot()
+    tracemalloc.stop()
+    for stat in snap.statistics("lineno"):
+        print(stat)
+
+
+def report_memory_matpy():
+    print("### Memory allocations of matpy implementation ###")
+    tracemalloc.start()
+    m1 = matpy.Matrix.rand(2048 * 2, 2048 * 2)
+    snap = tracemalloc.take_snapshot()
+    tracemalloc.stop()
+    for stat in snap.statistics("lineno"):
+        print(stat)
+
+
 if __name__ == "__main__":
+    report_memory_matpy()
+    report_memory_vanilla()
+    """
     vmmul, vmadd = measure_vanilla()
     mmul, madd = measure_matpy()
 
-    print(f"Pyre-python matmul: {vmmul:.2f}s \t add: {vmadd:.2f}s")
-    print(f"matpy matmul matmul: {mmul:.2f}s \t add: {madd:.2f}s")
+    print(f"Pyre-python matmul: {vmmul:.2f}s \t add: {vmadd:.3f}s")
+    print(f"matpy matmul matmul: {mmul:.2f}s \t add: {madd:.3f}s")
     print(f"Speed up for matmul is {(vmmul / mmul):.1f}x and for add is {(vmadd / madd):.1f}x") 
+    """
 
 
